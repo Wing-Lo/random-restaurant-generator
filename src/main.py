6988edd4d0a9a3ad677e8a09ce0384ec1ab42d8d
wing-lo <15623@coderacademy.edu.au>
2024-05-04 00:41:53 +1000

    project initalised

---
+++ src/main.py
@@ -0,0 +1,7 @@
+def display_welcome_message():
+    print("Welcome to the Random Restaurant Generator!")
+    print("This tool will help you find a restaurant based on your preferences.")
+    print("Let's get started!\n")
+
+# Call the function to display the welcome message
+display_welcome_message()
